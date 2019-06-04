2f2fe405ca3ebe3a5267204cfad29368ddaf6eb4
jafaghpo <jafaghpo@student.42.fr>
2019-06-04 17:42:53 +0200

    implementation done, need to fix tick

--- www/index.js
+++ www/index.js
@@ -1,4 +1,33 @@
-import * as wasm from "wasm-game-of-life";
+import { init, Universe } from "wasm-game-of-life";
 
+const sleep = (ms) => new Promise(resolve => setTimeout(resolve, ms));
+// console.log("before init");
+init();
+// console.log("after init");
+let iteration = 0;
+let prev_map = null;
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new(10, 10);
 
-wasm.greet();
+const renderLoop = async () =>
+{
+	console.log("iteration: ", iteration++);
+	let map = universe.render();
+
+	console.log("prev");
+	console.log(prev_map);
+	console.log("map");
+	console.log(map);
+
+	pre.textContent = map
+	prev_map = map;
+	universe.tick();
+
+	await sleep(1000);
+	if (iteration < 100)
+	{
+		requestAnimationFrame(renderLoop);
+	}
+};
+
+requestAnimationFrame(renderLoop);
